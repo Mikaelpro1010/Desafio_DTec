69982f21f7265355c3d19a88e202096e40bbe3c9
Mikaelpro1010 <mkpro10@alu.ufc.br>
2023-08-15 16:11:32 -0300

    realizando modificações na lógica e base de dados

--- desafio.py
+++ desafio.py
@@ -1,29 +1,44 @@
-#vetores onde estão armazenados os valores já existentes e também onde será adicionados os novos dados
-code_employee = [15, 1, 26, 12, 8]
-name_employee = ["João da Silva", "Pedro Santos", "Maria Oliveira", "Rita Alcântara", "Ligia Matos"]
-cargos = [2500, 1500, 10000, 1200, 800]
-code_cargo = [1, 2, 3, 5, 2]
+employee_data = [
+    {"code": 15, "name": "João da Silva", "cargo": 1},
+    {"code": 1, "name": "Pedro Santos", "cargo": 2},
+    {"code": 26, "name": "Maria Oliveira", "cargo": 3},
+    {"code": 12, "name": "Rita Alcântara", "cargo": 2},
+    {"code": 8, "name": "Ligia Matos", "cargo": 2}
+]
 
-#função responsável por mostrar ao usário mostrando um relatório de informações dos usuários
+cargos = [
+    {"codigo": 0, "salario": 2500},
+    {"codigo": 1, "salario": 1500},
+    {"codigo": 2, "salario": 10000},
+    {"codigo": 3, "salario": 1200},
+    {"codigo": 4, "salario": 800}
+]
+
+#função responsável por exibir o nome, codigo de funcionario e salario de cada funcionário
 def exibirRelatorio():
+
     print("----------------------------------------------------")
     print("                   RELATÓRIO                        ")
     print("----------------------------------------------------")
-    for i in range(len(code_employee)):
-        for j in range(len(name_employee)):
-            if(i==j):
-                print("Nome:"+ name_employee[j] + ",codigo " + str(code_employee[i]))
+
+    #busca na base de dados os dados de cada funcionário
+    for employee in employee_data:
+        for cargo in cargos:
+            if employee["cargo"] == cargo["codigo"]:
+                print(f"Nome: {employee['name']}, Código: {employee['code']}, Salário: ${cargo['salario']}")
 
 #função tem como finalidade mostrar o salário total de todos os funcinários referente ao cargo informado
 def exibirSalarioTotal():
     total = 0
-    cargo = int(input("Informe o cargo que você deseja saber o valor total: "))
+    valor = int(input("Informe o cargo que você deseja saber o valor total: "))
 
-    for i in range(len(code_cargo)):
-        for j in range(len(cargos)):
-            if cargo == code_cargo[i] and cargo == j:
-                total += cargos[j] 
-    print("Os funcionários pertencentes ao cargo " + str(cargo) + " recebem um total de $" + str(total))
+    #busca na base de dados os valores de salário referente ao cargo informado pelo usuário
+    for employee in employee_data:
+        for cargo in cargos:
+            if valor == employee["cargo"] and valor == cargo["codigo"]:
+                total+=cargo["salario"]
+    
+    print("Os funcionários pertencentes ao cargo " + str(valor) + " recebem um total de $" + str(total))
 
 
 #função que tem como objetivo cadastrar um novo tipo de cargo
@@ -31,34 +46,38 @@ def cadastrarCargo():
     print("------------------------------------------------------------------------")
     print("                   Cadastro de novo cargo                               ")
     print("------------------------------------------------------------------------")
-    salario = int(input("Informe o salário desejado para o novo cargo: "))
-    if(salario > 0):
-        novo_codigo = max(code_cargo) + 1 if code_cargo else 0  # Incrementar 1 ao último código existente
-        code_cargo.append(novo_codigo)  # Adicionar o novo código à lista
-        cargos.append(salario)  # Adicionar o novo salário à lista de cargos
+    salary = int(input("Informe o salário desejado para o novo cargo: "))
+    if(salary > 0):
+        #iterando pela lista "cargos" para encontrar o código máximo e, em seguida, incrementando-o em 1.
+        new_code_cargo = max(cargo["codigo"] for cargo in cargos) + 1
+
+        #armazenando os dados do novo cargo na base de dados
+        cargos.append({"codigo": new_code_cargo, "salario": salary })
+        
         print("Cadastro realizado com sucesso!")
     else:
-        print("Não foi possível realizar o cadastro, redirecionando você ao menu!")
+        print("Não foi possível realizar o cadastro, tente novamente!")
+        cadastrarCargo()
 
 #função que verifica se o código de funcionário já está sendo usado
-def checkExistingCode(new_code_employee, code_employee):
-    for i in range(len(code_employee)):
-        if new_code_employee == code_employee[i]:
+def checkExistingCode(new_code_employee):
+    for employee in employee_data:
+        if new_code_employee == employee["code"]:
             print("Não foi possível realizar o cadastro, codigo de funcionário já existe!")
             cadastrarFuncionario()
 
 #função que possui como finalidade checar se o cargo já existe na lista de cargos
-def checkExistingCodeCargo(new_cargo):
-    if new_cargo in code_cargo:
-        print("Usuario cadastrado com sucesso!")
-    else:
-        print("Não foi possível realizar o cadastro, o codigo do cargo informado não existe!")
-        cadastrarFuncionario()
+# def checkExistingCodeCargo(new_cargo):
+#     if new_cargo in code_cargo:
+#         print("Usuario cadastrado com sucesso!")
+#     else:
+#         print("Não foi possível realizar o cadastro!!")
+#         cadastrarFuncionario()
 
-def exibirCodigosCargos():
+def exibirCodigoCargo():
     print("                    Cargos Disponíveis                                  ")
-    for i in range(len(cargos)):
-        print(str(i) + "-" + str(cargos[i]))
+    for cargo in cargos:
+        print(str(cargo["codigo"]) + "- $" + str(cargo["salario"]))
 
 #função responsável por cadastrar um novo funcionário
 def cadastrarFuncionario():
@@ -67,39 +86,34 @@ def cadastrarFuncionario():
     print("------------------------------------------------------------------------")
 
     new_name = input("Informe o nome do seu funcinário: ")
+    new_code_employee = int(input("Informe o codigo do seu funcionário: "))
 
-    new_code_employee = int(input("Informe o codigo deste funcionário: "))
+    checkExistingCode(new_code_employee)
 
-    checkExistingCode(new_code_employee, code_employee)
+    exibirCodigoCargo()
 
-    exibirCodigosCargos()
+    new_cargo = int(input("Informe o cargo deste funcionário: "))
+    # checkExistingCodeCargo(new_cargo)
 
-    new_cargo = int(input("Informe o codigo do cargo deste funcionário: "))
-    checkExistingCodeCargo(new_cargo)
-
-    #usando a função append para adicionar os dados do novo funcionário em seus respectivos vetores
-    code_employee.append(new_code_employee)
-    name_employee.append(new_name)
-    code_cargo.append(new_cargo)
+    #armazenando os novos dados do funcionário na base de dados
+    employee_data.append({"code": new_code_employee, "name": new_name, "cargo": new_cargo})
 
     return new_name, new_code_employee, new_cargo
 
 #menu para selecionar a ação desejada pelo usuário
 def menu():
     print("1 - Cadastrar Cargo\n2 - Cadastrar Funcionário\n3 - Exibir Relatório\n4 - Exibir Salário Total")
-    opcao = input("Bem vindo, o que deseja fazer?")
+    option = input("Bem vindo, o que deseja fazer?")
 
-    #dependendo do numero escolhido pelo usuário, o mesmo será direcionado a função atrelada a este número
-    if opcao == "1":
+    if option == "1":
         cadastrarCargo()
-    elif opcao == "2":
+    elif option == "2":
         cadastrarFuncionario()
-    elif opcao == "3":
+    elif option == "3":
         exibirRelatorio()
-    elif opcao == "4":
+    elif option == "4":
         exibirSalarioTotal()
-
-#função principal do código, onde todas as funções serão chamadas aqui através da função menu
+    
 def main():
     continuar = True
 
